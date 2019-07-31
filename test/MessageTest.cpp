03da0197d1ca9f0a210cbb3ec8377cac5ffffeca
Esercizio <none@gmeil.com>
2019-07-31 20:15:33 +0200

    - the unit testing of the Message class has been modified in order to unify the style using the English language

--- test/MessageTest.cpp
+++ test/MessageTest.cpp
@@ -8,24 +8,24 @@
 
 TEST(Message, Constructor){
 
-    Message defaultReadMessage("Beatrice", "Arturo", "ho caricato la versione 1.6");
+    Message defaultReadMessage("Beatrice", "Arturo", "I have uploaded version 1.6");
 
     ASSERT_EQ("Beatrice", defaultReadMessage.getSender());
     ASSERT_EQ("Arturo", defaultReadMessage.getReceiver());
-    ASSERT_EQ("ho caricato la versione 1.6", defaultReadMessage.getText());
+    ASSERT_EQ("I have uploaded version 1.6", defaultReadMessage.getText());
     ASSERT_EQ(false, defaultReadMessage.isRead());
 
-    Message specificReadMessage("Arturo", "Beatrice", "dovrò fare il merge", true);
+    Message specificReadMessage("Arturo", "Beatrice", "i'll have to do merge", true);
 
     ASSERT_EQ("Arturo", specificReadMessage.getSender());
     ASSERT_EQ("Beatrice", specificReadMessage.getReceiver());
-    ASSERT_EQ("dovrò fare il merge", specificReadMessage.getText());
+    ASSERT_EQ("i'll have to do merge", specificReadMessage.getText());
     ASSERT_EQ(true, specificReadMessage.isRead());
 }
 
 TEST(Message, SettingRead){
 
-    Message m("unknown", "Marco", "ho sbagliato numero", false);
+    Message m("unknown", "Marco", "i dialed the wrong number", false);
     ASSERT_EQ(false, m.isRead());
 
     m.setRead(true);
